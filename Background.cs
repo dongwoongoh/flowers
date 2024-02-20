d8f86d4b2af763e4bbcb4a48e7f85887903bdf50
maaaaaaaaad <integral5137@gmail.com>
2024-02-21 02:40:18 +0900

    e

--- Background.cs
+++ Background.cs
@@ -1,18 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private readonly float moveSpeed = 3f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.down * moveSpeed * Time.deltaTime;
     }
 }
