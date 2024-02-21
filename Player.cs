7a328da5b41e6dffee4b948d62c73627771cc8de
maaaaaaaaad <integral5137@gmail.com>
2024-02-22 00:02:32 +0900

    e

--- Player.cs
+++ Player.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed;
+    [SerializeField] private float moveSpeed;
+    private CircleCollider2D circleCollider;
+
+    void Start()
+    {
+        circleCollider = GetComponent<CircleCollider2D>();
+
+    }
+
     void Update()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
-        Vector3 moveTo = new Vector3(horizontalInput, verticalInput, 0f);
-        transform.position += moveTo * moveSpeed * Time.deltaTime;
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0);
+        Vector3 newPosition = transform.position + movement * moveSpeed * Time.deltaTime;
+
+        Vector2 lowerLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        Vector2 upperRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+
+        float playerRadius = circleCollider.radius * Mathf.Max(transform.localScale.x, transform.localScale.y);
+
+        newPosition.x = Mathf.Clamp(newPosition.x, lowerLeft.x + playerRadius, upperRight.x - playerRadius);
+        newPosition.y = Mathf.Clamp(newPosition.y, lowerLeft.y + playerRadius, upperRight.y - playerRadius);
+
+        transform.position = newPosition;
     }
 }
